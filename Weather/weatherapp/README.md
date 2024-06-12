f517abc8da0ee92bcb1da0ce3cda93869bff11dc
xiaobao26 <xiaobao.xue@outlook.com>
2024-06-12 11:46:07 +1000

    finish the bg-image

--- Weather/weatherapp/README.md
+++ Weather/weatherapp/README.md
@@ -2,7 +2,7 @@
 ![alt text](image.png)
 
 ### File crate
-```plaintext
+```plaintext`
 - weatherapp
     -src
       -assets
@@ -20,3 +20,8 @@
             -WeatherCard.js
             -index.js
                 
+# Step2: 
+## Features
+    - To make the background color cover the full screen, using the class '**min-h-screen**' which sets the minimum height to 100vh (100% of the viewport height). This ensures that the    element will cover the entire height of the screen.
+    
+    
--- Weather/weatherapp/src/Component/BackgroundImage/BackgroundImage.js
+++ Weather/weatherapp/src/Component/BackgroundImage/BackgroundImage.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import backgroundImage from './background.png'; // Import the image
 
 const BackgroundImage = () => {
   return (
-    <div>
-      BackgroundImage
+    <div className="min-h-screen bg-gradient-to-r from-violet-500 to-violet-500" >
+      <img 
+        src={backgroundImage}
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          right: 0
+        }} 
+        alt='cloud image in the bottom-right' />
     </div>
   )
 }
